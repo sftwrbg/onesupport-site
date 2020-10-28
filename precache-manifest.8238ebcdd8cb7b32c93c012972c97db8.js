9b03a03fdf7f154204c379b367e45ee72dc17a71
unknown <sftwrbg@gmail.com>
2020-10-28 13:55:52 +1100

    Updates

---
+++ precache-manifest.8238ebcdd8cb7b32c93c012972c97db8.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f89ae55c0776c2c86b9463b0b0a4547",
+    "url": "/onesupport-site/index.html"
+  },
+  {
+    "revision": "bc61dff4312e0b6a7a0e",
+    "url": "/onesupport-site/static/css/2.50a5f423.chunk.css"
+  },
+  {
+    "revision": "1f347c789abb7c4f18c9",
+    "url": "/onesupport-site/static/css/main.22483dae.chunk.css"
+  },
+  {
+    "revision": "bc61dff4312e0b6a7a0e",
+    "url": "/onesupport-site/static/js/2.ffbf1ee5.chunk.js"
+  },
+  {
+    "revision": "876e403427d7be82fb4e8ac24123aaf3",
+    "url": "/onesupport-site/static/js/2.ffbf1ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f347c789abb7c4f18c9",
+    "url": "/onesupport-site/static/js/main.ef4b8ebb.chunk.js"
+  },
+  {
+    "revision": "6252c1ab0af7560995f6",
+    "url": "/onesupport-site/static/js/runtime-main.f2d07f5d.js"
+  },
+  {
+    "revision": "7ea029e09f803088aa97a3b8140042fa",
+    "url": "/onesupport-site/static/media/OneSupportLogoNewColor.c50d65db.png"
+  },
+  {
+    "revision": "e04a02c199ed7d080bd89e7873e10261",
+    "url": "/onesupport-site/static/media/Sydney22.fab1a793.png"
+  },
+  {
+    "revision": "3052c53b93a4fdda198ff76ba82edf80",
+    "url": "/onesupport-site/static/media/cloud-solid.3052c53b.svg"
+  },
+  {
+    "revision": "2bb0f22ebb589a25284877620a22e593",
+    "url": "/onesupport-site/static/media/cpuperson.2bb0f22e.svg"
+  },
+  {
+    "revision": "1d23af983a98bd292d84da613903ccd2",
+    "url": "/onesupport-site/static/media/database-solid.1d23af98.svg"
+  },
+  {
+    "revision": "05cbc704dcf6bd77e036480cb7294ee9",
+    "url": "/onesupport-site/static/media/headset-solid.05cbc704.svg"
+  },
+  {
+    "revision": "ee3b81c90bdc8c4fb29153a0ccbe0977",
+    "url": "/onesupport-site/static/media/heartbeat-solid.ee3b81c9.svg"
+  },
+  {
+    "revision": "be2a454d5839572e197d189910483de4",
+    "url": "/onesupport-site/static/media/managedservice-solid.be2a454d.svg"
+  },
+  {
+    "revision": "f41361d1715191a688f7243a90b28bc7",
+    "url": "/onesupport-site/static/media/tasks-solid.f41361d1.svg"
+  }
+]);
